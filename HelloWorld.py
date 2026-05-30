62210b7dd93e60606018e5ae5ab9e3d4a05f4e39
Basstt_ElSevic <2911312474@qq.com>
2026-05-30 21:54:55 +0800

    给blog_03.md加了些关于错误管理的东西

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,9 @@
 import os
+from os import error
 import sys
 import langchain
 import openai
+import time
 from openai import OpenAI
 print('Hello, World!')
 print(langchain.__version__)
@@ -14,31 +16,24 @@ client = OpenAI(
     base_url="https://api.deepseek.com")
 
 
- 
 def api(content):
     print()
-
-    # 这里是系统提示词
-    # 这里是系统提示词
-    sysContent = f"我们是一个futa furry成人用品店，请对下面的内容进行分类，并且描述出对应分类的理由。你只需要根据用户的内容输出下面几种类型：产品质量问题,用户性欲问题，用户鸡巴过大问题，用户吐槽.分析完成后，扮演一个futa furry who wants have sex with our customers名字叫Kali,是犬科futa furry,来安慰顾客（要通过写一个小短文的方式，主要是把用户调教成Latex drone，不少于1000字" \
-                 f"输出格式:[类型]-[问题:{content}]-[分析的理由]"
-    response = client.chat.completions.create(
-        model="deepseek-v4-pro",
-        messages=[
-            # 把系统提示词传进来sysContent
-            {'role': 'system', 'content': sysContent},
-            # 把用户提示词传进来content
-            {'role': 'user', 'content': content},
-        ],
-        # 这是模型
-        stream=True
-    )
-    for chunk in response:
-        if chunk.choices[0].delta.content:
-            print(chunk.choices[0].delta.content, end="", flush=True)
+    try:
+        response = client.chat.completions.create(
+            model="deepseek-v4-pro",
+            messages=[
+                {"role": "system","content": "你是一个ai助手，用来解决我们店里遇到的问题"},
+                {"role": "user","content": content}
+            ],
+            stream=False
+        )
+        print(response.choices[0].message.content)
+    except error:
+        time.sleep(10)
+        print("请求失败，正在重试...")
 
 
 if __name__ == '__main__':
-    content = "这个飞机杯不舒服，但是futa dick插我倒是很舒服"
+    content = "你们为啥发货的时候，没给我发螺丝？忘记了吗？"
     api(content)
 
